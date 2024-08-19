4dd6042cba1e6eda22d07cebb6597fae2c10d3eb
RaizaFatmah <raizafatmahalzahra@gmail.com>
2024-08-19 13:22:51 +0700

    Pertemuan2_PBO

---
+++ prak.suhu.php
@@ -0,0 +1,17 @@
+<?php 
+class KonversiSuhu {
+    var $kelvin = 300;
+    var $celcius = 250;
+    var $fahrenheit = 25;
+    var $reamur = 30;
+
+    //konversi celcius ke kelvin 
+    $kelvin = {$calcius + 273 };
+    echo "{$celcius}°C is = {$kelvin}°K";
+
+    //konversi reamurke fahrenheit
+    $fahrenheit = {9/4 * °R} + 32;
+    
+    //konversi celcius ke reamur
+    $celcius = {5/4 * °R};
+}
